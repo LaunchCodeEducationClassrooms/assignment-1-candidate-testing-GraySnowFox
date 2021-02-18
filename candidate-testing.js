4c1f5d2a1b8e48771304c6cc5b9bcd0cf6c59142
First Last <foxcrowlemon@gmail.com>
2021-02-18 18:45:13 +0000

    Finished assignment

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,18 +8,37 @@ let candidateName;
 let question;
 let correctAnswer;
 let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? \n",
+  "True or false: 5000 meters = 5 kilometers \n",
+  "(5 + 3)/2 * 10 = ? \n",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? \n",
+  "What is the minimum crew size for the ISS? \n"
+]
+let correctAnswers = [
+  "sally ride",
+  "true",
+  "40",
+  "trajectory",
+  "3"
+];
+let candidateAnswers= [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+  candidateName = input.question("What is your name?\n ");
 
 }
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+  for(let i = 0; i < questions.length; i++){
+    question = questions[i];
+    candidateAnswer = input.question(question);
+    candidateAnswer = candidateAnswer.toLowerCase();
+    candidateAnswers.push(candidateAnswer);
+  }
+
 
 
 }
@@ -27,9 +46,38 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  console.clear();
+  console.log("Candidate Name: " + candidateName + "\n");
+
+  let grade=0;
+  let rightAnswers=0;
+
+  for(let i = 0; i < candidateAnswers.length; i++){
+    console.log(questions[i]);
+    console.log("Your Answer: " + candidateAnswers[i]);
+    console.log("Correct Answer: " + correctAnswers[i] + "\n");
 
 
-  let grade;
+    if(candidateAnswers[i] === correctAnswers[i]){
+     
+      rightAnswers++;
+    }
+
+  }
+
+    grade = rightAnswers/correctAnswers.length * 100;
+
+    console.log(">>> Overall Grade: " + grade + "% " + "(" + rightAnswers + " of " +
+    correctAnswers.length + " responses correct) <<<<");
+
+    if(grade >= 80){
+      console.log(">>> Status: PASSED <<<");
+    } else {
+      console.log(">>> Status: FAILED <<<");
+    }
+
+
+
   
 
   return grade;
